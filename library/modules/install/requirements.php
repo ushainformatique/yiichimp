8d1759b59df164a767d49ad7e54d75a5df7d324f
Mayank Singhai <mayank.singhai@ushainformatique.com>
2018-09-27 12:20:49 +0530

    Fix php version

--- library/modules/install/requirements.php
+++ library/modules/install/requirements.php
@@ -9,9 +9,9 @@ return array(
     array(
         'name' => 'PHP version',
         'mandatory' => true,
-        'condition' => version_compare(PHP_VERSION, '5.4.0', '>='),
+        'condition' => version_compare(PHP_VERSION, '5.5.0', '>='),
         'by' => '<a href="http://www.yiiframework.com">Yii Framework</a>',
-        'memo' => 'PHP 5.4.0 or higher is required.',
+        'memo' => 'PHP 5.5 or higher is required.',
     ),
     array(
         'name' => 'Reflection extension',
